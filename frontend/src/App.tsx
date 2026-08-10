a5755211b3470c1531b80aeea508490bcf3363c0
Gianmarco Cesar Lazo Davila <glazodavila@ml.com>
2026-08-10 15:55:20 -0500

    fix: redirect raiz a login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Embajador from './pages/Embajador'
 import Asesor from './pages/Asesor'
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/embajador" element={<Embajador />} />
         <Route path="/asesor" element={<Asesor />} />
