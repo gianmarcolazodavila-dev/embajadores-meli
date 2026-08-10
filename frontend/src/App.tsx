862549144321a48a997a699625b0a043b719c757
Gianmarco Cesar Lazo Davila <glazodavila@ml.com>
2026-08-10 15:54:16 -0500

    fix: hashrouter para github pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Embajador from './pages/Embajador'
 import Asesor from './pages/Asesor'
@@ -6,13 +6,13 @@ import AuthVerify from './pages/AuthVerify'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/embajadores-meli">
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/embajador" element={<Embajador />} />
         <Route path="/asesor" element={<Asesor />} />
         <Route path="/auth/verify" element={<AuthVerify />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
